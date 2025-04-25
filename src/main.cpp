a632eb7d5f4caf0214aee6ad5caa372b88323fb6
Ogawa-jpg <uuogawa@ifdl.jp>
2025-04-25 16:10:34 +0900

    +Left & Right

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,39 @@
 #include <Arduino.h>
+#include "M5Unified.h"
 
-// put function declarations here:
-int myFunction(int, int);
+int a = 10;
+int b = 10;
+int x,y;
+int w,h;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  M5.begin();
+  M5.Display.print("Hello, M5Unified!\n");
+  
+  w = M5.Display.width();
+  h = M5.Display.height();
+  x = 0;
+  y = h-b;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  M5.update();
+  if(M5.BtnA.wasPressed()) {
+    M5.Lcd.clear();
+    if(x > 10){
+      x -= 10;
+    }
+    M5.Lcd.fillRect(x, y, a, b, TFT_RED);
+  }
+  if(M5.BtnC.wasPressed()) {
+    M5.Lcd.clear();
+    if(x < w - a){
+      x += 10;
+    }
+    M5.Lcd.fillRect(x, y, a, b, TFT_RED);
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-
-// This is a comment
